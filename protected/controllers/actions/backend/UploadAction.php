56ace34b688abb939cf21dabcfc3a77d893f6fc0
denis.khodakovskiy <andrewverner85@gmail.com>
2016-02-24 17:02:15 +0300

    info test

---
+++ protected/controllers/actions/backend/UploadAction.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: denis.khodakovskiy
+ * Date: 24-Feb-16
+ * Time: 4:54 PM
+ */
+class UploadAction extends CAction
+{
+
+    public function run($type)
+    {
+        echo $type;
+    }
+
+}
