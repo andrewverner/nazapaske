e2b79ba91fc912ebdf21079c27b1fa64b471a7c7
denis.khodakovskiy <andrewverner85@gmail.com>
2016-02-24 12:17:41 +0300

    info test

--- protected/controllers/actions/disk/InfoAction.php
+++ protected/controllers/actions/disk/InfoAction.php
@@ -16,40 +16,9 @@ class InfoAction extends CAction
         $name = str_replace('_',' ',$name);
         $producer = DiskProducer::model()->findByAttributes(['name' => $producer]);
         if (!$producer) throw new CHttpException(404);
-        echo $producer->id;
-        die();
         $model = WheelsDisk::model()->findByAttributes(['producer' => $producer->id, 'name' => $name]);
         if ($model != null) {
-            $details = new stdClass;
-            $details->producerName = $model->getProducerName();
-            $details->width = $model->getInterval('width');
-            $details->diameter = $model->getInterval('diameter');
-            $details->pcd = $model->getInterval('mounting_holes');
-            $details->pcd2 = $model->getInterval('mounting_holes_diameter');
-            $details->et = $model->getInterval('outreach');
-            $details->dco = $model->getInterval('central_hole_diameter');
-            $details->color = $model->getColors();
-            $details->producer = DiskProducer::model()->findByPk($model->producer);
-
-            $cr = new CDbCriteria;
-            $params = array(
-                ':name' => $model->name
-            );
-            $cr->addCondition("name=:name");
-            foreach (WheelsDisk::getFilters() as $field => $title) {
-                if (isset($_REQUEST[$field]) && $_REQUEST[$field] != '' && $_REQUEST[$field] != 0) {
-                    $cr->addCondition("$field=:$field");
-                    $params[":$field"] = $_REQUEST[$field];
-                }
-            }
-            $cr->params = $params;
-            $cr->order = 'diameter, width ASC';
-
-            $this->controller->render('details',array(
-                'model' => $model,
-                'details' => $details,
-                'list' => WheelsDisk::model()->findAll($cr)
-            ));
+            echo $model->name;
         } else
             throw new CHttpException(404);
     }
