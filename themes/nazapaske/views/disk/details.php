6d76092a2e1fc58af39e72841ad29e6ebd8081b5
dkhodakovskiy <andrewverner85@gmail.com>
2016-09-08 10:21:01 +0300

    VCredit fix

--- themes/nazapaske/views/disk/details.php
+++ themes/nazapaske/views/disk/details.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="http://credit-cart.su/services/easycredit/inc.js"></script>
+<script type="text/javascript" src="https://v-credit.su/services/easycredit/inc.js"></script>
 <div class="breadcrumbs">
     <?php
     $bread = ($model->producer == 2) ? array(
